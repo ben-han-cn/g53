f0d742a1f55c7b073c51a2be25e99b7db602397c
枫锦 <fengjin.hf@alibaba-inc.com>
2020-11-04 16:16:51 +0800

    expose attribute in edns subnet

--- ednssubnet.go
+++ ednssubnet.go
@@ -12,34 +12,34 @@ const (
 )
 
 type SubnetOpt struct {
-	family uint16
-	mask   uint8
-	scope  uint8
-	ip     net.IP
+	Family uint16
+	Mask   uint8
+	Scope  uint8
+	Ip     net.IP
 }
 
 func (subnet *SubnetOpt) Rend(render *MsgRender) {
 	render.WriteUint16(EDNS_SUBNET)
-	ipLen := uint(subnet.mask / 8)
-	if subnet.mask%8 != 0 {
+	ipLen := uint(subnet.Mask / 8)
+	if subnet.Mask%8 != 0 {
 		ipLen += 1
 	}
 
 	render.WriteUint16(uint16(2 + 2 + ipLen))
-	render.WriteUint16(subnet.family)
-	render.WriteUint8(subnet.mask)
-	render.WriteUint8(subnet.scope)
+	render.WriteUint16(subnet.Family)
+	render.WriteUint8(subnet.Mask)
+	render.WriteUint8(subnet.Scope)
 	var ipToWrite net.IP
-	if subnet.family == 1 {
-		ipToWrite = subnet.ip.To4().Mask(net.CIDRMask(int(subnet.mask), net.IPv4len*8))
+	if subnet.Family == 1 {
+		ipToWrite = subnet.Ip.To4().Mask(net.CIDRMask(int(subnet.Mask), net.IPv4len*8))
 	} else {
-		ipToWrite = subnet.ip.To16().Mask(net.CIDRMask(int(subnet.mask), net.IPv6len*8))
+		ipToWrite = subnet.Ip.To16().Mask(net.CIDRMask(int(subnet.Mask), net.IPv6len*8))
 	}
 	render.WriteData([]byte(ipToWrite)[0:ipLen])
 }
 
 func (subnet *SubnetOpt) String() string {
-	return fmt.Sprintf("; CLIENT-SUBNET: %s/%d/%d\n", subnet.ip.String(), subnet.mask, subnet.scope)
+	return fmt.Sprintf("; CLIENT-SUBNET: %s/%d/%d\n", subnet.Ip.String(), subnet.Mask, subnet.Scope)
 }
 
 //read from OPTION-LENGTH
@@ -65,10 +65,10 @@ func subnetOptFromWire(buf *util.InputBuffer) (Option, error) {
 	}
 
 	if ip != nil {
-		return &SubnetOpt{family: family,
-			mask:  mask,
-			scope: scope,
-			ip:    ip}, nil
+		return &SubnetOpt{Family: family,
+			Mask:  mask,
+			Scope: scope,
+			Ip:    ip}, nil
 	} else {
 		return nil, fmt.Errorf("unkown family")
 	}
@@ -93,10 +93,10 @@ func subnetOptFromRdata(rdata Rdata) Option {
 func (e *EDNS) AddSubnetV4(ip_ string) error {
 	if ip := net.ParseIP(ip_); ip != nil {
 		e.Options = append(e.Options, &SubnetOpt{
-			family: 1,
-			mask:   32,
-			scope:  0,
-			ip:     ip,
+			Family: 1,
+			Mask:   32,
+			Scope:  0,
+			Ip:     ip,
 		})
 		return nil
 	} else {
